c5e9db2701b74b97409192c3d8c328a3f4731f69
Vidura Priyadarshana <vidura200427@gmail.com>
2026-02-18 16:16:33 +0530

    Add CardEncryptionUtil for data encryption, decryption, and card number masking

---
+++ server/src/main/java/edu/epic/cms/util/CardEncryptionUtil.java
@@ -0,0 +1,62 @@
+package edu.epic.cms.util;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Component
+public class CardEncryptionUtil {
+
+    @Value("${app.encryption.secret-key}")
+    private String secretKey;
+
+    @Value("${app.encryption.algorithm}")
+    private String algorithm;
+
+    private static String SECRET_KEY;
+    private static String ALGORITHM;
+
+    @PostConstruct
+    public void init() {
+        SECRET_KEY = secretKey;
+        ALGORITHM = algorithm;
+    }
+
+    public static String encrypt(String data) {
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            byte[] encryptedBytes = cipher.doFinal(data.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error encrypting data", e);
+        }
+    }
+
+    public static String decrypt(String encryptedData) {
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error decrypting data", e);
+        }
+    }
+
+    public static String maskCardNumber(String cardNumber) {
+        if (cardNumber == null || cardNumber.length() <= 8) {
+            return "****";
+        }
+        String firstFour = cardNumber.substring(0, 4);
+        String lastFour = cardNumber.substring(cardNumber.length() - 4);
+        int middleLength = cardNumber.length() - 8;
+        return firstFour + "*".repeat(middleLength) + lastFour;
+    }
+}
