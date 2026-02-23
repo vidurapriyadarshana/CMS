5b31cd4da6744a3ee045f57ffafceffe0c501021
Vidura Priyadarshana <vidura200427@gmail.com>
2026-02-23 11:18:11 +0530

    feat: Implement User model with fields for userName, status, and name

---
+++ server/src/main/java/edu/epic/cms/model/User.java
@@ -0,0 +1,39 @@
+package edu.epic.cms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String userName;
+
+    private String status;
+
+    private String name;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
